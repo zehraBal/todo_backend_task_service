d8dab8671c2a7d0f24ada89fcc0c48bab03e78ec
zehraBal <fz.bal00@gmail.com>
2025-08-04 00:03:23 +0300

    fix(task): add @RequestHeader annotation to AuthServiceClient
    - Explicitly declare 'Authorization' header in Feign client

--- src/main/java/com/bal/task_service/client/AuthServiceClient.java
+++ src/main/java/com/bal/task_service/client/AuthServiceClient.java
@@ -2,10 +2,11 @@ package com.bal.task_service.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "AUTH-SERVICE")
 public interface AuthServiceClient  {
 
     @GetMapping("/api/auth/validate-token")
-    Long getUserIdFromToken(String token);
+    Long getUserIdFromToken(@RequestHeader("Authorization") String token);
 }
